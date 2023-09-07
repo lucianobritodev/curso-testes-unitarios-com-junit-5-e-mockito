f1c84ded9d767be1004de8827717f40d2c0c3d29
luciano <luciano.guedes@foton.la>
2023-09-07 00:31:07 -0300

    feat: Application - Project Created - Criado projeto - Adicionado dependência do Dev-Tools - Adicionado dependência do Lombok - Adicionado dependência do JPA - Adicionado dependência do Postgres - Adicionado dependência do Jakarta Validation Constraints - Adicionado configurações no Server, Logs, Hikari e Datasource - Adicionado conexão com Banco de Dados - Adicionado Dev Profile

---
+++ src/main/java/com/lucianobrito/cursotestesunitarioscomjunit5emockito/CursoTestesUnitariosComJunit5EMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.lucianobrito.cursotestesunitarioscomjunit5emockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoTestesUnitariosComJunit5EMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursoTestesUnitariosComJunit5EMockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucianobrito/cursotestesunitarioscomjunit5emockito/CursoTestesUnitariosComJunit5EMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucianobrito.cursotestesunitarioscomjunit5emockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoTestesUnitariosComJunit5EMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
