6988bcd52968136c70e9fa79c65d93f9bfed1b19
luciano <luciano.guedes@foton.la>
2023-09-07 01:20:11 -0300

    Refactor: Resource - Call Service - Adicionado injeção de dependência do UserService - Refatorado findById para chamar o UserService

--- src/main/java/com/lucianobrito/cursotestesunitarioscomjunit5emockito/resources/UserResource.java
+++ src/main/java/com/lucianobrito/cursotestesunitarioscomjunit5emockito/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.lucianobrito.cursotestesunitarioscomjunit5emockito.resources;
 
 import com.lucianobrito.cursotestesunitarioscomjunit5emockito.domain.User;
+import com.lucianobrito.cursotestesunitarioscomjunit5emockito.services.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,11 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@AllArgsConstructor
 public class UserResource {
+
+    private UserService userService;
 
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id) {
-        return ResponseEntity.ok(new User(id, "Maria", "maria@email.com","123"));
+        return ResponseEntity.ok(userService.findById(id));
     }
 
 }
